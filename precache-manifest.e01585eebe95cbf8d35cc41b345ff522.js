0645ed8979b1dbeaa07c5a0594193b4bd70b12a0
unknown <ricktx04@gmail.com>
2021-01-14 17:53:48 -0600

    Updates

---
+++ precache-manifest.e01585eebe95cbf8d35cc41b345ff522.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e82b9e1e527caea4f90742a445c293e",
+    "url": "/react-mastermind-game.github.io/index.html"
+  },
+  {
+    "revision": "b015619d11edc065c48a",
+    "url": "/react-mastermind-game.github.io/static/css/main.7425a5c3.chunk.css"
+  },
+  {
+    "revision": "ebc4fd4e299d5c942fbd",
+    "url": "/react-mastermind-game.github.io/static/js/2.182b2cc1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-mastermind-game.github.io/static/js/2.182b2cc1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b015619d11edc065c48a",
+    "url": "/react-mastermind-game.github.io/static/js/main.02bdc1d1.chunk.js"
+  },
+  {
+    "revision": "8ecec3dc09e5592f9411",
+    "url": "/react-mastermind-game.github.io/static/js/runtime-main.1c23886f.js"
+  }
+]);
