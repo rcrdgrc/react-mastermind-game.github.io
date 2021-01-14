60bf545880bf70665b7d9dab38ede8cdb02c18aa
unknown <ricktx04@gmail.com>
2021-01-14 17:38:15 -0600

    deploy to gitpage

---
+++ routes/api/scores.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router();
+const scoresCtrl = require('../../controllers/scores');
+
+/*-- Protected Routes --*/
+router.get('/', scoresCtrl.highScores);
+router.post('/', checkAuth, scoresCtrl.create);
+
+/*--  Helper Functions --*/
+function checkAuth(req, res, next) {
+  if (req.user) return next();
+  return res.status(401).json({msg: 'Not Authorized'});
+}
+
+module.exports = router;
